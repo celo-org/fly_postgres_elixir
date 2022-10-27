d8ad960b58dc48fc0ef7cd5dc6e7d76f44cabe55
Donald Hutchison <donald.hutchison@clabs.co>
2022-10-27 13:24:04 +0200

    Downgrade telemetry.

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule FlyPostgres.MixProject do
     [
       {:fly_rpc, "~> 0.1.5"},
       {:postgrex, ">= 0.0.0"},
-      {:telemetry, "~> 1.0"},
+      {:telemetry, "~> 0.4.3"},
       {:ex_doc, "~> 0.25", only: :dev}
     ]
   end
